cf7f96df1ec0421063e46e871a102a74e7a0e6c2
judy <judy@tadatuta.com>
2016-05-14 10:17:37 +0300

    Добавление анимации, мелкие доработки

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
       var popup = document.querySelector(".modal");
-      // var elements = popup.querySelectorAll(".form-elem");
-      // var elements_array = Array.prototype.slice.call(elements);
 
       var tip1 = popup.querySelector("#tip1");
       var tip2 = popup.querySelector("#tip2");
       var tip3 = popup.querySelector("#tip3");
-
-      // var tips = popup.querySelectorAll(".tip");
-      // var tips_array = Array.prototype.slice.call(tips);
 
       var link = document.querySelector(".btn-feedback");
       var close = document.querySelector(".form-feedback-exit");
@@ -26,21 +21,30 @@
       close.addEventListener("click", function(event) {
           event.preventDefault();
           popup.classList.remove("modal-show");
+          form.classList.remove("modal-error");
       });
 
       window.addEventListener("keydown", function(event) {
         if (event.keyCode === 27) {
           if (popup.classList.contains("modal-show")) {
             popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
           }
         }
       });
 
       form.addEventListener("submit", function(event) {
-              if (!username.value || !email.value || !text.value) {
+          if (!username.value || !email.value || !text.value) {
             event.preventDefault();
-            console.log("Нужно ввести все данные.");
-          }
+            // console.log("Нужно ввести все данные.");
+            form.classList.remove("modal-error");
+            form.offsetWidth = popup.offsetWidth;
+            form.classList.add("modal-error");
+            } 
+          else {
+            localStorage.setItem("username", username.value);
+            localStorage.setItem("email", email.value);
+            }
       });
 
 
